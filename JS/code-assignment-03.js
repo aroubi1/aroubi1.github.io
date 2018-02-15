e0d441abc08725ba33b329c889202852cff42668
aroubi1 <35343414+aroubi1@users.noreply.github.com>
2018-02-15 10:33:24 -0600

    updated

---
+++ JS/code-assignment-03.js
@@ -0,0 +1,6 @@
+let projectId = 1
+let projectTitle = "Earthquake Map"
+let  thumbnail = true
+let projectTitles = ["Earthquake Map", "Map of Earthquakes magnitude of 5 or higher"]
+
+console.log(projectId)
