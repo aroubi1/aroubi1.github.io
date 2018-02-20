a01a9d2804227811db480bac089c7690708c6144
aroubi1 <35343414+aroubi1@users.noreply.github.com>
2018-02-20 11:41:02 -0600

    updated

--- JS/code-assignment-03.js
+++ JS/code-assignment-03.js
@@ -1,14 +1,14 @@
 // assign values to a set of variables representing a project's metadata
 let projectId = 1
-let projectUrl = 'https://www.arcgis.com/apps/MapTour/index.html?appid=a06edc3270f44f62b04921dc931895e9'
-let projectTitle = 'Tour of Mobile Alabama'
-let projectDesc = 'An ArcGIS Online Web App showing a tour of the Mobile, Alabama area including the RSA Tower and Battleship Memorial Park.'
+let projectUrl = "https://www.arcgis.com/apps/MapTour/index.html?appid=a06edc3270f44f62b04921dc931895e9"
+let projectTitle = "Tour of Mobile Alabama"
+let projectDesc = "An ArcGIS Online Web App showing a tour of the Mobile, Alabama area including the RSA Tower and Battleship Memorial Park."
 let projectThumb = true
 let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
 
 // display messages in the browser's developer console, combining the values with text
 console.log('ID: ' + projectId)
-console.log('Url: ' + projectUrl)
+console.log('URL: ' + projectUrl)
 console.log('Title: ' + projectTitle)
 console.log('Description: ' + projectDesc)
 console.log('Has thumbnail? ' + projectThumb)
